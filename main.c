c5e71d50d4867f32835c88c40611c6976f6f9f17
Joaquim Alves <joaquim.junior@redes.unb.br>
2019-09-13 15:04:59 -0300

    Mudança de threads para processos

--- main.c
+++ main.c
@@ -1,34 +1,41 @@
-#include <pthread.h>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 
-void wait(void)
-{
-    sleep(2);
+void processo_pai(){
+    int *statusfilho, *statusneto, *statuspai;
+    int pidfilho;
+    if (( pidfilho = fork()) < 0) {
+        printf("erro no fork\n");
+        exit(1);
+    }else if (pidfilho == 0){
+    	processo_filho(pidfilho);
+   
+    }else{
+        wait(statusfilho); //espera o processo filho encerrar
+        printf("Sou o processo %d pai, sejam bem vindos", pidpai);
+        exit(1);
+    }
 
-    printf("Done.\n");
 }
 
-
-int main(){
-    pthread_t thread_pai;
-    int err;
-
-    err = pthread_create(&thread_pai, NULL, wait, NULL);
-
-    if (err)
-    {
-        printf("An error occured: %d", err);
-        return 1;
-    }
-        
-    printf("Waiting for the thread to end...\n");
-
-    pthread_join(thread_pai, NULL);
-
-    printf("Thread ended.\n");  
+int processo_filho(int pidfilho){
+    printf("Ola processo %d neto sou o processo %d filho", pidneto, pidfilho);
+    sleep(5);
     return 0;
 }
 
-/* Seguindo o tutorial do site
-https://dev.to/quantumsheep/basics-of-multithreading-in-c-4pam */
+int processo_neto(int pidneto){
+    printf("Sou processo %d neto", pidneto);
+    return 0;
+}
+
+int main(){
+   processo_pai();
+
+
+}
